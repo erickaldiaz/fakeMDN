f0751e14977ddb04d720e9e028982377d2c9dc54
Lucas Livchits <livchitsl@gmail.com>
2019-06-04 22:38:02 -0300

    Agrega función fakeReduce

--- app.js
+++ app.js
@@ -19,3 +19,23 @@ function fakeEvery(array, callback) {
   //si se cumple la condición en todos los elementos del array, devuelve true
   return true;
 }
+
+function fakeReduce(array, callback) {
+  //declara una variable en la que se va a ir guardando el resulado de la función callback
+  let result;
+  
+  //se itera el array, sin condición para que se itere "siempre", ya que la condición va dentro del for
+  for (let i = 0; ; i++) {
+    //si el array tiene solo un elemento, se devuelve ese elemento 
+    if (array.length == 1) {
+      return array[0];
+    //si el array tiene solo dos elementos, se devuelve el resultado de la función callback con esos dos elementos
+    } else if (array.length == 2) {
+          return callback(array[0], array[1]);
+    //si el array tiene más de dos elementos, se ejecuta la función callback con los primeros dos elementos, y luego se los reemplaza por el resultado de la función			
+    } else {
+        result = callback(array[0], array[1]);
+        array.splice(0, 2, result);
+    }
+  }
+}
